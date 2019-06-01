e5e76556d09c85bdbc953e7eb3750acef14ca629
BillHu <bill@146-254.eduroam.rwth-aachen.de>
2019-06-01 13:37:28 +0200

    uebung8 num2

---
+++ nu2uebung8.m
@@ -0,0 +1,60 @@
+f = @(x) 1./(1 + x.^2);
+L = [5,10,40];
+I = [-5,5];
+figure;
+
+for index = 1 : 3
+    n = L(index);
+    subplot(1,3,index);
+    fplot(f,I,'k:');
+    hold on;
+    i = 0 : 1 : n;
+    x = -5 + 10*i/n;
+    y = f(x);
+    p = polyfit(x,y,n);
+    interpol = @(x) polyval(p,x);
+    fplot(interpol,I);
+    ylim([-5,1.5]);
+    title(['equidist IP n = ',num2str(n)]);
+    legend('f','IP');
+end
+
+format long;
+figure;
+for index = 1 : 3
+    n = L(index);
+    subplot(1,3,index);
+    fplot(f,I,'k:');
+    hold on;
+    i = 0 : 1 : n;
+    x = 5 * cos((2*i+1)/(2*n+2)*pi);
+    y = f(x);
+    p = polyfit(x,y,n);
+    interpol = @(x) polyval(p,x);
+    fplot(interpol,I);
+    ylim([-5,1.5]);
+    title(['tscheby points IP n = ',num2str(n)]);
+    legend('f','IP');
+end
+
+figure;
+for index = 1 : 3
+    n = L(index);
+    subplot(1,3,index);
+    hold on;
+    i = 0 : 1 : n;
+    
+    x1 = -5 + 10*i/n;
+    p1 = poly(x1);
+    x2 = 5 * cos((2*i+1)/(2*n+2)*pi);
+    p2 = poly(x2);
+  
+    knotenpoly1 = @(x) polyval(p1,x);
+    fplot(knotenpoly1,I);
+%     ylim([-5,1.5]);
+    knotenpoly2 = @(x) polyval(p2,x);
+    fplot(knotenpoly2,I);
+    t = title(['knotenpoly n = ',num2str(n)],'Units','normalized','Position',[0.68,1.01,0]);
+    
+    legend('equidist','tscheby');
+end
